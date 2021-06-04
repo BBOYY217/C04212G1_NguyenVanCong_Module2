427e657ca75dbdaca2203fe94021b81993932561
unknown <nguyenvancong2107@gmail.com>
2021-06-04 18:42:07 +0700

    upbaimoi

---
+++ C04212G1_NguyenVanCong_Module_2/src/Bai_01_introduction_to_java/exercise/ExchangeMoney.java
@@ -0,0 +1,13 @@
+package Bai_01_introduction_to_java.exercise;
+
+import java.util.Scanner;
+
+public class ExchangeMoney {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your USD");
+        float USD = Float.parseFloat(scanner.nextLine());
+        float VND = 23000 * USD;
+        System.out.println("Vnd = " + VND);
+    }
+}
---
+++ C04212G1_NguyenVanCong_Module_2/src/Bai_02_loop_in_java/exercise/Show20Element.java
@@ -0,0 +1,29 @@
+package Bai_02_loop_in_java.exercise;
+
+import java.util.Scanner;
+
+public class Show20Element {
+    public static void main(String[] args) {
+        int number;
+        int count = 0;
+        int N = 2;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số cho number");
+        number = scanner.nextInt();
+        while (count < number) {
+            boolean check = true;
+            for (int i = 2; i < N; i++) {
+                if (N % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                System.out.println( N);
+                count++;
+            }
+            N++;
+        }
+    }
+}
